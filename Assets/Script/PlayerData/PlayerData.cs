4a14b5f0c774f184dcee765f909972e6d48dbcaf
HongPing <hongping1224@gmail.com>
2020-10-20 20:27:18 +0800

    playerdata 修改

--- Assets/Script/PlayerData/PlayerData.cs
+++ Assets/Script/PlayerData/PlayerData.cs
@@ -4,7 +4,41 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
+
+    bool HaveSaveFile = false;
+    int GameProgress;
+    int FoodAmount;
+    int PowerData;
     public void Awake()
+    {
+        if (!HaveSaveFile)
+        {
+            Init();
+        }
+        ReadData();
+    }
+    public void OnApplicationQuit()
+    {
+        SaveData();
+    }
+    private void ReadData()
+    {
+        GameProgress = PlayerPrefs.GetInt("GameProgress");
+        FoodAmount = PlayerPrefs.GetInt("FoodAmount");
+        PowerData = PlayerPrefs.GetInt("PowerData");
+    }
+
+    public void SaveData()
+    {
+        //game progess
+        PlayerPrefs.SetInt("GameProgress", GameProgress);
+        //food amount
+        PlayerPrefs.SetInt("FoodAmount", FoodAmount);
+        //power data
+        PlayerPrefs.SetInt("PowerData", PowerData);
+    }
+
+    public void Init()
     {
         //game progess
         PlayerPrefs.SetInt("GameProgress", 0);
