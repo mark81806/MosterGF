405572405f72a8aec008dff618711406e4516c8d
mark81806 <72798415+mark81806@users.noreply.github.com>
2020-10-20 20:14:04 +0800

    Add files via upload

---
+++ Assets/Script/GameData/GameData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+    //chapter //chapter unlock request
+    public int[] chapter_unlock_request = new int[] { };
+    //power   //power level unlock request,powerups,power increase rate
+    public int[] power_level__unlock_request = new int[] { };
+    public float power_increase_rate_online = 1.0f;
+    public float power_increase_rate_offline = 0.2f;
+    //food    //food class//food spawn amount
+    public int[] food_class = new int[] { };
+    //ad
+    public static GameData self;
+    public void Awake()
+    {
+        self = this;
+    }
+
+}
---
+++ Assets/Script/PlayerData/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public void Awake()
+    {
+        //game progess
+        PlayerPrefs.SetInt("GameProgress", 0);
+        //food amount
+        PlayerPrefs.SetInt("FoodAmount", 0);
+        //power data
+        PlayerPrefs.SetInt("PowerData", 0);
+    }
+}
