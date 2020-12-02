a304811f087c818e50a254ac6c60f4d6efeaacb4
mark81806 <72798415+mark81806@users.noreply.github.com>
2020-12-02 17:02:22 +0800

    12/2 clear

--- Assets/Script/DialogueTree/ClickToStart.cs
+++ Assets/Script/DialogueTree/ClickToStart.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NodeCanvas.DialogueTree;
+using NodeCanvas.DialogueTrees;
 
 public class ClickToStart : MonoBehaviour
 {
