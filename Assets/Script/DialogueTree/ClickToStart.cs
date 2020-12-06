5ae70182f599575ad9ae34e907d2f77734d09375
mark81806 <72798415+mark81806@users.noreply.github.com>
2020-12-06 16:20:58 +0800

    24

--- Assets/Script/DialogueTree/ClickToStart.cs
+++ Assets/Script/DialogueTree/ClickToStart.cs
@@ -7,11 +7,14 @@ public class ClickToStart : MonoBehaviour
 {
 
     public DialogueTreeController dialogueController;
-    private string DialoguePath = "Dialogues";
+    private const string DialoguePath = "Dialogues/D123";
 
+    public void Start() 
+    {
+        //dialogueController.graph = Resources.Load(DialoguePath);
+    }
     public void Click()
     {
-        //dialogueController.graph = Resources.Load(DialoguePath + "test1-1");
         dialogueController.StartDialogue();
     }
 }
---
+++ Assets/Script/PlayerData/PlayerBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBar : MonoBehaviour
+{
+    public Text ChapText;
+    public void update() 
+    {
+       // PlayerData.self.
+    }
+}
