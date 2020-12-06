2ea5cc62e8bcd039fec2ceabb32f5acd3e7b2bac
mark81806 <72798415+mark81806@users.noreply.github.com>
2020-12-06 13:58:43 +0800

    12/6

--- Assets/Script/DialogueTree/ClickToStart.cs
+++ Assets/Script/DialogueTree/ClickToStart.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.UI;
 using NodeCanvas.DialogueTrees;
 
 public class ClickToStart : MonoBehaviour
 {
-    
+
+    public DialogueTreeController dialogueController;
+    private string DialoguePath = "Dialogues";
+
+    public void Click()
+    {
+        //dialogueController.graph = Resources.Load(DialoguePath + "test1-1");
+        dialogueController.StartDialogue();
+    }
 }
---
+++ Assets/Script/SceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void StoryMode() 
+    {
+        SceneManager.LoadScene(0);
+    } 
+    public void MainScene() 
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void HuntMode() 
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
